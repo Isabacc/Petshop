bdbffa5bfaa18e11bf7e535949e659dae255e04f
Isabacc <135075817+Isabacc@users.noreply.github.com>
2023-08-18 04:47:11 +0100

    conteudo e estilização do header e footer

--- assets/js/components.js
+++
@@ -1,8 +0,0 @@
-async function loadComponent(pathHMTL, domElement) {
-    let arq = await fetch(pathHMTL);
-    let component = await arq.text();
-    
-    document.querySelector(domElement).insertAdjacentHTML("beforeend", component);
-}
-
-export { loadComponent };
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,11 @@
-import { loadComponent } from "./components.js";
+async function loadComponent(pathHMTL, domElement) {
+    let arq = await fetch(pathHMTL);
+    let component = await arq.text();
+    
+    document.querySelector(domElement).insertAdjacentHTML("beforeend", component);
+}
 
 loadComponent("assets/components/footer.html", "footer");
 loadComponent("assets/components/header.html", "header");
+
+
